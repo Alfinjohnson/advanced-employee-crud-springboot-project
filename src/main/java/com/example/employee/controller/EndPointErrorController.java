872ef4151fb773dd4a36a357c7f32df54bc7869c
AJ <cad01alfin@gmail.com>
2023-07-15 22:31:21 +0530

    added OpenApi 3 documentation, Swagger UI

--- src/main/java/com/example/employee/controller/EndPointErrorController.java
+++ src/main/java/com/example/employee/controller/EndPointErrorController.java
@@ -1,6 +1,7 @@
 package com.example.employee.controller;
 
 import com.example.employee.utility.ApiResponse;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ public class EndPointErrorController implements ErrorController {
     /**
      * @apiNote Invalid endpoint request error handling methods */
     @RequestMapping(ERROR_PATH)
+    @Hidden
     public ResponseEntity<ApiResponse<String>> handleError() {
         // Custom error response
         String errorMessage = "Invalid endpoint requested";
